d5c3b8856169328a943c0a61c548367cd3c11847
vinay <vinayuppada_20523@aitpune.edu.in>
2024-02-26 09:55:55 +0530

    fvffg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import imgs from './images.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      { imgs && imgs.map((item)=>{
+        return(
+          <div key={item.id}>
+            <img src={item.img} alt=""/>
+          </div>
+        )
+      })
+      }
     </div>
   );
 }
